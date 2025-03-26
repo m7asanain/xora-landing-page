76c0666902a0493a558fdab7f7addb090c10965c
Mustafa Hasanain <m7asanain@gmail.com>
2025-03-26 17:35:29 +0300

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </main>
+  )
+}
+
+export default App
